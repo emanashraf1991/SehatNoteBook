5f08b708bc744e323cbfe4a4c33c30fe7ecff5a9
emanashraf <emanashraf1991@gmail.com>
2023-08-07 23:59:06 +0300

    Register & Login

--- SehatNoteBook.Entities/obj/Debug/net8.0/SehatNoteBook.Entities.AssemblyInfo.cs
+++ SehatNoteBook.Entities/obj/Debug/net8.0/SehatNoteBook.Entities.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SehatNoteBook.Entities")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6827525b4c1582f73a7b8b8ccff4a42a658421f6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("SehatNoteBook.Entities")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SehatNoteBook.Entities")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
